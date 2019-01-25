ca028f502ab78945b51c1a6fe3385f49ddd3bdcb
tan <demonblack789@gmail.com>
2019-01-25 21:32:52 +0700

    java

--- Java.js
+++ Java.js
@@ -1,31 +1,50 @@
                 "use strict"
                 var submit      = document.getElementById("submit");
-                var test        = document.getElementsByClassName("Need_Test");
+                var test        = document.getElementsByClassName("need_Test");
                 var i           = 0;
                 var display     = document.getElementsByClassName("invalid");
-                var border      = document.getElementsByClassName("border");
+                var border      = document.getElementsByClassName("check");
                 var email       = document.getElementById("email")
-                submit.addEventListener("click", Test_form);
-                email.addEventListener("click",Test_email);
-                function Test_form()
+                var check       = document.getElementsByClassName("choose") 
+                submit.addEventListener("click", test_form);
+                submit.addEventListener("click",test_Email);
+                submit.addEventListener("click",choose);
+                function test_form()
                 {
                     for(i = 0; i < test.length;i++){
                         
-                        if(test[i].value === "" ){
-                            border[i].style.border = " 1px solid red ";
-                            display[i].style.display = "inline";
+                        if(test[i].value.trim() === "" ){
+                            border[i].style.border = " 2px solid red ";
+                            display[i].style.visibility = "visible";
                         }
                         else{
-                            border[i].style.border = " 1px solid green";
-                            display[i].style.display = "none";
+                            border[i].style.border = " 2px solid green";
+                            display[i].style.visibility = "hidden";
                         }
                     }
                 }
-                function Test_email()
+                function test_Email()
                 {   
-                    if(email.value === "")
+                    if(email.value.trim() === "")
                     {
-                        
+                        document.getElementById("email").style.border = "2px solid red";
+                        document.getElementById("Checked_Gmail").style.visibility = "visible";
                     }
+                    else{
+                        document.getElementById("email").style.border = "2px solid green";
+                        document.getElementById("Checked_Gmail").style.visibility = "hidden";
+                    }
+            
                 }
+                function choose(){
+                    for(var i = 0 ; i < check.length ; i++){
+                        if(check[i].value == "Choose..."){
+                            check[i].style.border = "2px solid red";
+                        }
+                        else{
+                            check[i].style.border = "2px solid green";
+                        }
+                    }
+                
+                }      
 
